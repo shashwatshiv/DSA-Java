0a6016a0035f7737932ae3d843fb0af7a8f924ab
Shashwat Shivhare <shashwat02infinity@gmail.com>
2023-08-21 14:42:43 +0530

    dsa

---
+++ PracticeCode/src/subArray.java
@@ -0,0 +1,17 @@
+public class subArray {
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8,10};
+        sub(arr);
+    }
+    public static void sub(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length ; j++) {
+                for (int k = i; k <= j ; k++) {
+                    System.out.printf(arr[k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
